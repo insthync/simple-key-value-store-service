88e935d29fe124f50c47f5d54dd2e19cfa28d9ac
Insthync <ittipon.bay@gmail.com>
2021-05-17 01:10:22 +0700

    Fix auth is not function error

--- index.ts
+++ index.ts
@@ -4,138 +4,142 @@ import bearerAuthPlugin from 'fastify-bearer-auth'
 import { PrismaClient } from '@prisma/client'
 import * as dotenv from "dotenv"
 
-const server = fastify()
 const prisma = new PrismaClient()
 dotenv.config()
 
 const passphrases: string = process.env['PASSPHRASES']!
-server.register(authPlugin)
-server.register(bearerAuthPlugin, {
-    keys: JSON.parse(passphrases),
-    addHook: false,
-})
 
-server.get('/:ownerId', async (request, reply) => {
-    const params : any = request.params
-    const ownerId = params.ownerId
-    const entries = await prisma.entry.findMany({
-        where: {
-            ownerId: ownerId
-        },
-        orderBy: [{
-            key: 'asc'
-        }]
+const server = fastify()
+    .register(authPlugin)
+    .register(bearerAuthPlugin, {
+        keys: JSON.parse(passphrases),
+        addHook: false,
     })
-    reply.code(200).send(entries)
-})
+    .after(() => {
 
-server.get('/:ownerId/:key', async (request, reply) => {
-    const params : any = request.params
-    const ownerId = params.ownerId
-    const key = params.key
-    const entry = await prisma.entry.findFirst({
-        where: {
-            ownerId: ownerId,
-            key: key
-        }
-    })
-    reply.code(200).send(entry)
-})
-
-server.post('/', {
-    preHandler: server.auth([
-        server.verifyBearerAuth!
-    ]),
-}, async (request, reply) => {
-    // Create or set value 
-    const body : any = request.body
-    const ownerId = body.ownerId
-    const key = body.key
-    const value = body.value
-    let entry = await prisma.entry.findFirst({
-        where: {
-            ownerId: ownerId,
-            key: key
-        }
-    })
-    if (entry) {
-        entry = await prisma.entry.update({
-            where: { id: Number(entry.id) },
-            data: {
-                ownerId: ownerId,
-                key: key,
-                value: String(value)
-            }
+        server.get('/:ownerId', async (request, reply) => {
+            const params : any = request.params
+            const ownerId = params.ownerId
+            const entries = await prisma.entry.findMany({
+                where: {
+                    ownerId: ownerId
+                },
+                orderBy: [{
+                    key: 'asc'
+                }]
+            })
+            reply.code(200).send(entries)
         })
-    } else {
-        entry = await prisma.entry.create({
-            data: {
-                ownerId: ownerId,
-                key: key,
-                value: String(value)
-            }
-        })
-    }
-    reply.code(200).send(entry)
-})
 
-server.patch('/', {
-    preHandler: server.auth([
-        server.verifyBearerAuth!
-    ]),
-}, async (request, reply) => {
-    // Increase value if value type is numeric, set value if value type is string
-    const body : any = request.body
-    const ownerId = body.ownerId
-    const key = body.key
-    let value = body.value
-    let entry = await prisma.entry.findFirst({
-        where: {
-            ownerId: ownerId,
-            key: key
-        }
-    })
-    if (entry) {
-        if (!isNaN(Number(entry.value)) && !isNaN(Number(value))) {
-            value = Number(entry.value) + Number(value)
-        }
-        entry = await prisma.entry.update({
-            where: { id: Number(entry.id) },
-            data: {
-                ownerId: ownerId,
-                key: key,
-                value: String(value)
-            }
+        server.get('/:ownerId/:key', async (request, reply) => {
+            const params : any = request.params
+            const ownerId = params.ownerId
+            const key = params.key
+            const entry = await prisma.entry.findFirst({
+                where: {
+                    ownerId: ownerId,
+                    key: key
+                }
+            })
+            reply.code(200).send(entry)
         })
-    } else {
-        entry = await prisma.entry.create({
-            data: {
-                ownerId: ownerId,
-                key: key,
-                value: String(value)
-            }
-        })
-    }
-    reply.code(200).send(entry)
-})
 
-server.delete('/', {
-    preHandler: server.auth([
-        server.verifyBearerAuth!
-    ]),
-}, async (request, reply) => {
-    // Delete value
-    const body : any = request.body
-    const ownerId = body.ownerId
-    const key = body.key
-    await prisma.entry.deleteMany({
-        where: {
-            ownerId: ownerId,
-            key: key
-        }
+        server.post('/', {
+            preHandler: server.auth([
+                server.verifyBearerAuth!
+            ]),
+        }, async (request, reply) => {
+            // Create or set value 
+            const body : any = request.body
+            const ownerId = body.ownerId
+            const key = body.key
+            const value = body.value
+            let entry = await prisma.entry.findFirst({
+                where: {
+                    ownerId: ownerId,
+                    key: key
+                }
+            })
+            if (entry) {
+                entry = await prisma.entry.update({
+                    where: { id: Number(entry.id) },
+                    data: {
+                        ownerId: ownerId,
+                        key: key,
+                        value: String(value)
+                    }
+                })
+            } else {
+                entry = await prisma.entry.create({
+                    data: {
+                        ownerId: ownerId,
+                        key: key,
+                        value: String(value)
+                    }
+                })
+            }
+            reply.code(200).send(entry)
+        })
+
+        server.patch('/', {
+            preHandler: server.auth([
+                server.verifyBearerAuth!
+            ]),
+        }, async (request, reply) => {
+            // Increase value if value type is numeric, set value if value type is string
+            const body : any = request.body
+            const ownerId = body.ownerId
+            const key = body.key
+            let value = body.value
+            let entry = await prisma.entry.findFirst({
+                where: {
+                    ownerId: ownerId,
+                    key: key
+                }
+            })
+            if (entry) {
+                if (!isNaN(Number(entry.value)) && !isNaN(Number(value))) {
+                    value = Number(entry.value) + Number(value)
+                }
+                entry = await prisma.entry.update({
+                    where: { id: Number(entry.id) },
+                    data: {
+                        ownerId: ownerId,
+                        key: key,
+                        value: String(value)
+                    }
+                })
+            } else {
+                entry = await prisma.entry.create({
+                    data: {
+                        ownerId: ownerId,
+                        key: key,
+                        value: String(value)
+                    }
+                })
+            }
+            reply.code(200).send(entry)
+        })
+
+        server.delete('/', {
+            preHandler: server.auth([
+                server.verifyBearerAuth!
+            ]),
+        }, async (request, reply) => {
+            // Delete value
+            const body : any = request.body
+            const ownerId = body.ownerId
+            const key = body.key
+            await prisma.entry.deleteMany({
+                where: {
+                    ownerId: ownerId,
+                    key: key
+                }
+            })
+            reply.code(200).send()
+        })
     })
-    reply.code(200).send()
-})
+
 
 server.listen(Number(process.env['PORT']), (err, address) => {
     if (err) {
